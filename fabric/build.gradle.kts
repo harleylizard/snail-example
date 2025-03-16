645e9d92b1c41353e629eeb6ceeba432ec8a555b
harleylizard <64570529+harleylizard@users.noreply.github.com>
2025-03-16 12:26:02 +1000

    cleanup

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -3,10 +3,6 @@ import soul.software.snail.dependency.snail
 group = "com.harleylizard"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     snail {
         `kotlin-minecraft`.include
